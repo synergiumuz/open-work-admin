8520cbebfa59916bde15b4c3ffa1359bdb087b6b
Kkkiittt <khamidov357@gmail.com>
2023-03-12 16:59:57 +0500

    api helper

---
+++ OpenWorkAdmin.Service/Services/Common/ApiHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using OpenWorkAdmin.Service.Utils;
+
+namespace OpenWorkAdmin.Service.Services.Common;
+
+public class ApiHelper
+{
+	private readonly HttpClient _client = new();
+	private readonly string ip = "http://65.2.141.175";
+
+	public async Task<RequestResult> DeleteAsync(string query)
+	{
+		var request = await _client.DeleteAsync(ip + query);
+		return new RequestResult(await request.Content.ReadAsStringAsync(), request.StatusCode);
+	}
+
+	public async Task<RequestResult> GetAsync(string query)
+	{
+		var request = await _client.GetAsync(ip + query);
+		return new RequestResult(await request.Content.ReadAsStringAsync(), request.StatusCode);
+	}
+
+	public async Task<RequestResult> PostAsync<T>(string query, T obj) where T : class
+	{
+		var request = await _client.PostAsJsonAsync(ip + query, obj);
+		return new RequestResult(await request.Content.ReadAsStringAsync(), request.StatusCode);
+	}
+
+	public async Task<RequestResult> PutAsync<T>(string query, T obj) where T : class
+	{
+		var request = await _client.PutAsJsonAsync(ip + query, obj);
+		return new RequestResult(await request.Content.ReadAsStringAsync(), request.StatusCode);
+	}
+}
---
+++ OpenWorkAdmin.Service/Utils/RequestResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace OpenWorkAdmin.Service.Utils;
+
+public class RequestResult
+{
+	public string Content { get; set; } = string.Empty;
+	public HttpStatusCode StatusCode { get; set; }
+
+	public RequestResult(string content, HttpStatusCode statusCode)
+	{
+		Content = content;
+		StatusCode = statusCode;
+	}
+}
