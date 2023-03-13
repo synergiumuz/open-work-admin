691ccaa766938707a30312d428f9706200b2159a
Kkkiittt <khamidov357@gmail.com>
2023-03-13 16:10:12 +0500

    comment service

---
+++ OpenWorkAdmin.Service/Interfaces/ICommentService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using OpenWork.Services.ViewModels.Comments;
+
+namespace OpenWorkAdmin.Service.Interfaces;
+
+public interface ICommentService
+{
+	public Task<IEnumerable<CommentViewModel>> GetByUserAsync(long userId);
+	public Task<IEnumerable<CommentViewModel>> GetByWorkerAsync(long workerId);
+	public Task<bool> DeleteAsync(long id);
+}
